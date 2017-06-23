b8035154c25d3e37fbcd0143b9fd21736449ffa1
jotase <jota.segovia@gmail.com>
2017-06-23 19:54:43 -0400

    patch para que funcione en rails 4.1, reemplazando los find_each por each, y el ActiveRecord::Relation.empty? a to_a.empty?

--- lib/acts_as_xlsx/ar.rb
+++ lib/acts_as_xlsx/ar.rb
@@ -11,25 +11,23 @@ module Axlsx
   # === Overview
   # This module defines the acts_as_xlsx class method and provides to_xlsx support to both AR classes and instances
   module Ar
-
     def self.included(base) # :nodoc:
       base.send :extend, ClassMethods
     end
 
     # Class methods for the mixin
     module ClassMethods
-
       # defines the class method to inject to_xlsx
       # @option options [Array, Symbol] columns an array of symbols defining the columns and methods to call in generating sheet data for each row.
       # @option options [String] i18n (default nil) The path to search for localization. When this is specified your i18n.t will be used to determine the labels for columns.
       # @example
       #       class MyModel < ActiveRecord::Base
       #          acts_as_xlsx :columns=> [:id, :created_at, :updated_at], :i18n => 'activerecord.attributes'
-      def acts_as_xlsx(options={})
+      def acts_as_xlsx(options = {})
         cattr_accessor :xlsx_i18n, :xlsx_columns
         self.xlsx_i18n = options.delete(:i18n) || false
         begin
-          self.xlsx_columns = options.delete(:columns) ||  self.column_names.map { |c| c = c.to_sym }
+          self.xlsx_columns = options.delete(:columns) || column_names.map { |c| c = c.to_sym }
         rescue ActiveRecord::StatementInvalid
         end
         extend Axlsx::Ar::SingletonMethods
@@ -38,7 +36,6 @@ module Axlsx
 
     # Singleton methods for the mixin
     module SingletonMethods
-
       # Maps the AR class to an Axlsx package
       # options are passed into AR find
       # @param [Array, Array] columns as an array of symbols or a symbol that defines the attributes or methods to render in the sheet.
@@ -50,43 +47,40 @@ module Axlsx
       # @option options [String] name This will be used to name the worksheet added to the package. If it is not provided the name of the table name will be humanized when i18n is not specified or the I18n.t for the table name.
       # @see Worksheet#add_row
       def to_xlsx(options = {})
-
         row_style = options.delete(:style)
         header_style = options.delete(:header_style) || row_style
         types = [options.delete(:types) || []].flatten
 
-        i18n = options.delete(:i18n) || self.xlsx_i18n
-        columns = options.delete(:columns) || self.xlsx_columns
+        i18n = options.delete(:i18n) || xlsx_i18n
+        columns = options.delete(:columns) || xlsx_columns
         labels = options.delete(:labels) || {}
 
         p = options.delete(:package) || Package.new
         row_style = p.workbook.styles.add_style(row_style) unless row_style.nil?
         header_style = p.workbook.styles.add_style(header_style) unless header_style.nil?
-        i18n = self.xlsx_i18n == true ? 'activerecord.attributes' : i18n
+        i18n = xlsx_i18n == true ? 'activerecord.attributes' : i18n
         sheet_name = options.delete(:name) || (i18n ? I18n.t("#{i18n}.#{table_name.underscore}") : table_name.humanize)
         data = options.delete(:data) || where(options)
 
-        return p if data.empty?
+        return p if data.to_a.empty?
 
-        p.workbook.add_worksheet(:name=>sheet_name) do |sheet|
-
+        p.workbook.add_worksheet(name: sheet_name) do |sheet|
           col_labels = columns.map do |c|
-                         default = c.to_s.gsub(/\./, '_').humanize
-                         if labels[c]
-                           labels[c]
-                         elsif i18n
-                           I18n.t("#{i18n}.#{self.name.underscore}.#{c}", :default => default)
-                         else
-                           default
-                         end
-                       end
+            default = c.to_s.tr('.', '_').humanize
+            if labels[c]
+              labels[c]
+            elsif i18n
+              I18n.t("#{i18n}.#{name.underscore}.#{c}", default: default)
+            else
+              default
+            end
+          end
 
-          sheet.add_row col_labels, :style=>header_style
+          sheet.add_row col_labels, style: header_style
 
-          iterator = data.respond_to?(:find_each) ? [:find_each, {:batch_size=> 500}] : [:each]
-          data.send(*iterator) do |r|
+          data.each do |r|
             row_data = columns.map do |c|
-              if r.attributes.has_key? c
+              if r.attributes.key? c
                 r[c]
               elsif c.to_s =~ /\./
                 v = r; c.to_s.split('.').each { |method| v = v.nil? ? nil : v.send(method) }; v
@@ -94,7 +88,7 @@ module Axlsx
                 r.send(c)
               end
             end
-            sheet.add_row row_data, :style=>row_style, :types=>types
+            sheet.add_row row_data, style: row_style, types: types
           end
         end
         p
@@ -105,5 +99,3 @@ end
 
 require 'active_record'
 ActiveRecord::Base.send :include, Axlsx::Ar
-
-
